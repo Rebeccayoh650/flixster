5de06178b1b88ed1128384dc9abb3305fe23d336
Rebeccayoh650 <ryohannes2@dons.usfca.edu>
2021-02-12 21:36:51 -0800

    new changes

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCellTableViewCell.swift
+//  flixster
+//
+//  Created by rebecca yohannes on 2/10/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

6782d269669413b0d0fe464162126d1fb138356a
Rebeccayoh650 <ryohannes2@dons.usfca.edu>
2021-02-19 18:35:22 -0800

    first commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by rebecca yohannes on 2/18/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
